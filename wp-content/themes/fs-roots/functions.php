54547a6f2a0e5e666ef3b305eac34b09bd599ef7
Manuel Hotz <manuel.hotz@enplotz.de>
2012-08-09 01:51:45 +0200

    Fixed deprecated call in roots_entry_meta function

--- wp-content/themes/fs-roots/functions.php
+++ wp-content/themes/fs-roots/functions.php
@@ -72,5 +72,5 @@ add_action('widgets_init', 'roots_register_sidebars');
 // return post entry meta information
 function roots_entry_meta() {
   echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. sprintf(__('%s', 'roots'), get_the_date(), get_the_time()) .'</time>';
-  echo '<p class="byline author vcard">'. __('by', 'roots') .' <a href="'. get_author_posts_url(get_the_author_meta('id')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
+  echo '<p class="byline author vcard">'. __('by', 'roots') .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
 }
