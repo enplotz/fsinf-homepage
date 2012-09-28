7740fa696af94db8480c7c79ca3130d757a52efc
Manuel Hotz <manuel.hotz@enplotz.de>
2012-09-28 16:30:03 +0200

    Added (empty) Admin Page for Stammtisch-Tool

--- wp-content/plugins/stammtisch-tool/stammtisch-tool.php
+++ wp-content/plugins/stammtisch-tool/stammtisch-tool.php
@@ -7,8 +7,30 @@ Version: 0.1
 Author URI: http://enplotz.de
 */
 
+
+/*
+ *  DATABASE STUFF
+ */
+define ('STAMMTISCH_DEFAULT_REQUIRED', 3);
+define ('STAMMTISCH_DEFAULT_TIME', '20:00:00');
+/* Sunday 0, Saturday 6 */
+define ('STAMMTISCH_DEFAULT_DAY', 3);
+define ('STAMMTISCH_DEFAULT_LOCATION', 'Defne');
+define ('STAMMTISCH_DEFAULT_URL', 'http://defnekn.de');
+
+
+$stammtisch_options =
+  array(
+    "stammtisch_required" => STAMMTISCH_DEFAULT_REQUIRED,
+    "stammtisch_time"     => STAMMTISCH_DEFAULT_TIME,
+    "stammtisch_day"      => STAMMTISCH_DEFAULT_DAY,
+    "stammtisch_location" => STAMMTISCH_DEFAULT_LOCATION,
+    "stammtisch_url"      => STAMMTISCH_DEFAULT_URL
+  );
+
+
 function stammtisch_install (){
-  global $wpdb;
+  global $wpdb, $stammtisch_options;
 
   $table_name = $wpdb->prefix . "stammtisch";
 
@@ -21,7 +43,57 @@ function stammtisch_install (){
 
   require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
   dbDelta($sql);
+
+  foreach ($stammtisch_options as $name => $default) {
+    add_option($name, $default);
+  }
+}
+
+register_activation_hook(__FILE__,'stammtisch_install');
+/**********************************************************************/
+
+/*
+ * ADMIN MENU
+ */
+
+add_action('admin_menu', 'stammtisch_add_admin_page');
+
+function stammtisch_add_admin_page()
+{
+  add_menu_page(__('Stammtisch-Tool', 'stammtisch-tool'), __('Stammtisch-Tool', 'stammtisch-tool'), 'manage_options', 'stammtisch-tool', 'stammtisch_admin_page');
+}
+
+function stammtisch_admin_page()
+{
+  ?>
+    <div class="wrap">
+        <div class="icon32" id="icon-options-general"></div>
+        <h2>Stammtisch Tool Administration</h2>
+        <form action="options.php" method="post">
+            <p class="submit">
+                <input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Changes','wptuts_textdomain'); ?>" />
+            </p>
+        </form>
+    </div><!-- wrap -->
+<?php
+}
+
+function stammtisch_register_settings()
+{
+  global $stammtisch_options;
+
+  foreach ($stammtisch_options as $key => $value) {
+    register_setting('stammtisch_settings', $key, 'stammtisch_validate_options');
+  }
+}
+
+function stammtisch_validate_options($input)
+{
+  /* Do nothing until we have time for validation :P */
+  return $input;
 }
 
 
-register_activation_hook(__FILE__,'stammtisch_install');
+
+
+
