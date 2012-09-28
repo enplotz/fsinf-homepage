be91e92933f6c8e3148cfebe1ec418a3c41f4383
Manuel Hotz <manuel.hotz@enplotz.de>
2012-09-28 15:15:49 +0200

    Stammtisch-Tool als Plugin, anlegen der Datenbanktabelle

--- wp-content/plugins/stammtisch-tool/stammtisch-tool.php
+++ wp-content/plugins/stammtisch-tool/stammtisch-tool.php
@@ -7,104 +7,21 @@ Version: 0.1
 Author URI: http://enplotz.de
 */
 
-class Stammtisch_Tool extends WP_Widget {
-
- function Stammtisch_Tool() {
-   $widget_ops = array(
-              'classname' => 'stammtischtool',
-              'description' => 'Specify a regulars table meeting.');
-
-   $control_ops = array(
-              'width' => 250,
-              'height' => 500,
-              'id_base' => 'stammtischtool-widget');
-
-   $this->WP_Widget('stammtischtool-widget', 'Stammtisch-Tool', $widget_ops, $control_ops );
- }
-
-function form ($instance) {
-
-  $defaults = array('minparticipants' => '3','regulars_date'=>'Tuesday, 20:00','title'=>'Stammtisch', 'place'=>'', 'place_url'=>'');
-  $instance = wp_parse_args( (array) $instance, $defaults ); ?>
-
-  <p>
-      <label for="<?php echo $this->get_field_id('title'); ?>">Title:</label>
-      <input type="text" name="<?php echo $this->get_field_name('title') ?>" id="<?php echo $this->get_field_id('title') ?> " value="<?php echo $instance['title'] ?>" size="20">
-  </p>
-
-    <p>
-      <label for="<?php echo $this->get_field_id('regulars_date'); ?>">Date (Day, Time):</label>
-      <input type="text" name="<?php echo $this->get_field_name('regulars_date') ?>" id="<?php echo $this->get_field_id('regulars_date') ?> " value="<?php echo $instance['regulars_date'] ?>" size="20">
-  </p>
-  
-    <p>
-      <label for='<?php echo $this->get_field_id('minparticipants'); ?>'>Min. required No. of Participants:</label>
-      <select id="<?php echo $this->get_field_id('minparticipants'); ?>" name="<?php echo $this->get_field_name('minparticipants'); ?>">
-        <?php for ($i=1;$i<=20;$i++) {
-          echo '<option value="'.$i.'"';
-          if ($i==$instance['minparticipants']) echo ' selected="selected"';
-          echo '>'.$i.'</option>';
-        } ?>
-      </select>
-    </p>
-
-    <p>
-      <label for="<?php echo $this->get_field_id('place'); ?>">Place:</label>
-      <input type="text" name="<?php echo $this->get_field_name('place') ?>" id="<?php echo $this->get_field_id('place') ?> " value="<?php echo $instance['place'] ?>" size="20">
-  </p>
-
-  <p>
-      <label for="<?php echo $this->get_field_id('place_url'); ?>">Place URL:</label>
-      <input type="text" name="<?php echo $this->get_field_name('place_url') ?>" id="<?php echo $this->get_field_id('place_url') ?> " value="<?php echo $instance['place_url'] ?>" size="20">
-  </p>
-
-  <?php
-}
-
-function update ($new_instance, $old_instance) {
-  $instance = $old_instance;
-
-  $instance['title'] = $new_instance['title'];
-  $instance['regulars_date'] = $new_instance['regulars_date'];
-  $instance['minparticipants'] = $new_instance['minparticipants'];
-  $instance['place'] = $new_instance['place'];
-  $instance['place_url'] = $new_instance['place_url'];
-
-  return $instance;
-}
-function widget ($args,$instance) {
-  extract($args);
-
-  $title = $instance['title'];
-  $regulars_date = $instance['regulars_date'];
-  $minparticipants = $instance['minparticipants'];
-  $place = $instance['place'];
-  $place_url = $instance['place_url'];
-
-  // retrieve posts information from database
+function stammtisch_install (){
   global $wpdb;
-  //$posts = get_posts('numberposts='.$numberposts.'&category='.$catid);
-  
-  $out = '<ul>';  
-  $out .= '<li>(# of '.$minparticipants.' needed)</li>';
-  $out .= '<li>Next Meeting: '.$regulars_date.'</li>';
-  $out .= '<li>Place: <a href="'.$place_url.'">'.$place.'</a></li>';
-  $out .= '</ul>';
 
-  //print the widget for the sidebar
-  echo $before_widget;
-  echo $before_title.$title.$after_title;
-  echo $out;
-  echo $after_widget;
-}
+  $table_name = $wpdb->prefix . "stammtisch";
 
-}
+  $sql = "CREATE TABLE  $table_name (
+            user_id BIGINT( 20 ) UNSIGNED NOT NULL,
+            date DATE NOT NULL,
+            arrives_later TINYINT( 1 ) NOT NULL,
+            PRIMARY KEY  (  user_id ,  date )
+          ) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;";
 
-function stammtischtool_load_widgets() {
-  register_widget('Stammtisch_Tool');
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  dbDelta($sql);
 }
 
 
-add_action('widgets_init', 'stammtischtool_load_widgets');
-
-?>
+register_activation_hook(__FILE__,'stammtisch_install');
