8de8dddbff0eb356ce1d09a4b671d2f12bc2f09e
Manuel Hotz <manuel.hotz@enplotz.de>
2012-12-05 23:17:13 +0100

    Fix author by line

--- wp-content/themes/fs-roots/functions.php
+++ wp-content/themes/fs-roots/functions.php
@@ -72,7 +72,8 @@ add_action('widgets_init', 'roots_register_sidebars');
 // return post entry meta information
 function roots_entry_meta() {
   echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. sprintf(__('%s', 'roots'), get_the_date(), get_the_time()) .'</time>';
-  echo '<p class="byline author vcard">'. __('by', 'roots') .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
+  // echo '<p class="byline author vcard">'. __('by', 'roots') .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
+  echo '<p class="byline author vcard">'.'<a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
 }
 
 function is_null_or_empty_string($var){
